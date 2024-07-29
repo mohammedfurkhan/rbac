7963b72de84a04162af98273f3dbfd17f6fd783c
mohammedfurkhan <mohdfurkhan5@gmail.com>
2024-07-29 16:15:57 -0400

    new val in tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.113.0"
+    }
+  }
+
+  required_version = ">= 0.12"
+}
+
 provider "azurerm" {
   features {}
 }
@@ -8,10 +19,4 @@ resource "azurerm_role_assignment" "example" {
   scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Application Developer"
   principal_id         = var.principal_id
-}
-
-resource "azurerm_policy_assignment" "example" {
-  name                 = "example-policy-assignment"
-  scope                = data.azurerm_subscription.primary.id
-  policy_definition_id = var.policy_definition_id
 }
--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "principal_id" {
   description = "The principal ID for role assignment"
   type        = string
 }
-
-variable "policy_definition_id" {
-  description = "The policy definition ID"
-  type        = string
-}
